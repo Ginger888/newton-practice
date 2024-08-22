53bc88defcb6996de7fe2a3145010ba558da826d
Jingjia Wang <jingjia_wang@berkeley.edu>
2024-08-22 14:36:39 -0700

    V2

--- newton.py
+++ newton.py
@@ -1,72 +1,30 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": 15,
-   "id": "94f2b529-d0a7-4d7b-b1ce-7d3b3f32912d",
-   "metadata": {},
-   "outputs": [
-    {
-     "name": "stdout",
-     "output_type": "stream",
-     "text": [
-      "-0.5984681383797152\n",
-      "3.2470121395045255\n"
-     ]
-    }
-   ],
-   "source": [
-    "import numpy as np\n",
-    "def drvt(f,x):\n",
-    "    eps = 0.00001\n",
-    "    print((f(x+eps)-f(x))/eps)\n",
-    "    return (f(x+eps)-f(x))/eps\n",
-    "\n",
-    "def drvt2(f,x):\n",
-    "    eps = 0.00001\n",
-    "    f2= ((f(x+eps*2)-f(x+eps))/eps - (f(x+eps)-f(x))/eps)/eps\n",
-    "    return f2\n",
-    "x = 2.5\n",
-    "f = np.cos\n",
-    "eps = 1\n",
-    "while eps > 0.0000001:\n",
-    "    eps = drvt(f,x)/drvt2(f,x)\n",
-    "    x = x- eps\n",
-    "    \n",
-    "\n",
-    "print(x)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "cd1c3689-bfe2-4aba-8bee-ad24eec5fa38",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "git clone https://github.com/<ginger888>/newton-practice"
-   ]
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.11.9"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+import numpy as np
+
+# find derivative of f
+def drvt(f,x):
+    eps = 1e-5
+    return (f(x+eps)-f(x))/eps
+
+def drvt2(f,x):
+    eps = 1e-5
+    f2 = (drvt(f,x+eps) - drvt(f,x))/eps
+    return f2
+
+
+def optimize(f,x,eps):
+    temp = 1 + eps
+    while abs(temp) > eps:
+        temp = drvt(f,x)/drvt2(f,x)
+        x = x - temp
+        print(abs(temp))
+        print(x)
+    return x
+
+
+# x = 2.5
+# f = np.cos
+# eps = 1e-15
+
+# optimize(f,x,eps)
+
+optimize(c
