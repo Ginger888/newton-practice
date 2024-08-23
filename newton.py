39a8d3755fb3cb706e7cf3c06de13208c8c3e116
Jingjia Wang <jingjia_wang@berkeley.edu>
2024-08-23 08:41:48 -0700

    V3

--- newton.py
+++ newton.py
@@ -5,26 +5,20 @@ def drvt(f,x):
     eps = 1e-5
     return (f(x+eps)-f(x))/eps
 
+# second derivative of f
 def drvt2(f,x):
     eps = 1e-5
     f2 = (drvt(f,x+eps) - drvt(f,x))/eps
     return f2
 
 
-def optimize(f,x,eps):
+def optimize(x,f):
+    if not callable(f):
+        raise TypeError(f"Argument is not a function, it is of type {type(f)}")
+    eps = 1e-15
     temp = 1 + eps
     while abs(temp) > eps:
         temp = drvt(f,x)/drvt2(f,x)
         x = x - temp
-        print(abs(temp))
-        print(x)
     return x
 
-
-# x = 2.5
-# f = np.cos
-# eps = 1e-15
-
-# optimize(f,x,eps)
-
-optimize(c
--- testNewton.py
+++ testNewton.py
@@ -8,7 +8,7 @@ import newton
 ## key as the output. 
 
 def test_basic_function():
-    assert np.isclose(newton.optimize(2.95, np.cos)['x'], math.pi)
+    assert np.isclose(newton.optimize(np.cos, 2.95)['x'], math.pi)
 
 def test_bad_input():
     with pytest.raises(TypeError):   
